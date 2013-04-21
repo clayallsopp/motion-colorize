4f16f508996d2fc7739fb72b1ad1692296249540
Clay Allsopp <clay.allsopp@gmail.com>
2013-04-21 00:12:57 -0700

    make the default text a little less intense

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,15 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     puts "This is blue".colorize(:blue)
-    puts "This is also blue".colorize( :color => :blue)
     puts "This is light blue".colorize(:light_blue)
-    puts "This is light blue with red background".colorize( :color => :light_blue, :background => :red )
-    puts "This is light blue with red background".colorize( :light_blue ).colorize( :background => :red )
-    puts "This is blue text on red".blue.on_red
-    puts "This is red on blue".colorize( :red ).on_blue
-    puts "This is red on blue and underline".colorize( :red ).on_blue.underline
-    puts "This is blue text on red".blue.on_red.blink
-    puts "This is uncolorized".blue.on_red.uncolorize
+    puts "This is light blue with white background".colorize( :color => :light_blue, :background => :white )
+    puts "This is blue on white and underline".colorize( :blue ).on_white.underline
+    puts "This is black and yellow black and yellow".yellow
+    puts "This is red".red.on_yellow
 
     true
   end
